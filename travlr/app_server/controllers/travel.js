34e0ff7d579ed7b2bda70c2f1a0b84afc6154086
COMPSECUR\donlu <jcollin62@yahoo.fr>
2026-07-21 01:58:54 -0500

    Module 3 Completed Baseline

--- travlr/app_server/controllers/travel.js
+++ travlr/app_server/controllers/travel.js
@@ -1,6 +1,14 @@
+const fs = require('fs');
+
+const trips = JSON.parse(
+    fs.readFileSync('./data/trips.json', 'utf8')
+);
+console.log('Trips loaded:', trips);
+
 const travel = (req, res) => {
     res.render('travel', {
-        title: 'Travlr Getaways'
+        title: 'Travlr Getaways',
+        trips
     });
 };
 
